5f2eb338c97f490a0d9083f1550af941889b8e8e
Sergii <sergiy.stotskiy@gmail.com>
2021-02-13 23:03:34 +0200

    bla

--- src/http/post-message/index.js
+++ src/http/post-message/index.js
@@ -2,16 +2,21 @@ const arc = require('@architect/functions')
 const data = require('@begin/data')
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.TL_TOKEN);
+console.log('create bot: ', process.env.TL_TOKEN)
+const bot = new TelegramBot(process.env.TL_TOKEN, {
+  webHook: true
+});
 
 bot.setWebHook(`https://rain-nem-staging.begin.app/message`);
 
-bot.onText(/\/ping/, (msg) => {
-  boy.sendMessage(msg.chat.id, 'pong');
+bot.on('message', (msg) => {
+  console.log('<--------')
+  bot.sendMessage(msg.chat.id, 'pong');
 })
 
 exports.handler = async function postMessage (req) {
   const body = arc.http.helpers.bodyParser(req);
+  console.log('<-- hook is called')
   bot.processUpdate(body);
 
   return {
