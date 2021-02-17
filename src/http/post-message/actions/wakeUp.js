af2edf37d422b07ab565c34ba5d178946e490820
Sergii <sergiy.stotskiy@gmail.com>
2021-02-17 23:32:36 +0200

    fix: prevents multiple same events from happening

--- src/http/post-message/actions/wakeUp.js
+++ src/http/post-message/actions/wakeUp.js
@@ -5,6 +5,16 @@ const { formatTime } = require('../services/date');
 async function exec(ctx) {
   const now = new Date().toISOString();
   const status = await ctx.regime.getStatusAt(now);
+
+  if (status.lastEvent && status.lastEvent.type === 'wakeUp') {
+    return ctx.reply(
+      'The last event is also of type #wakeUp. '
+      + 'Let\'s pretend that this never happened.\n\n'
+      + 'But if you wanted to change event\'s time, '
+      + 'then find the latest and reply to it with the correct time',
+    );
+  }
+
   const event = await ctx.regime.createEvent('wakeUp', { at: now });
   const prefix = `Woke up at *${formatTime(event.at)}*\\!`;
   const message = status
