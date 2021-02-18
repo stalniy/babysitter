2582c31583b77849f345c23e71040c5dcb7a025c
Sergii <sergiy.stotskiy@gmail.com>
2021-02-18 03:53:42 +0200

    fix: properly pass credentials directly to client

--- src/http/post-message/services/db.js
+++ src/http/post-message/services/db.js
@@ -1,12 +1,12 @@
-process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || process.env.BS_ACCESS_KEY_ID;
-process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || process.env.BS_SECRET_ACCESS_KEY;
-
 const { DynamoDB } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
 const db = new DynamoDB({
   region: 'eu-central-1',
-
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || process.env.BS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || process.env.BS_SECRET_ACCESS_KEY,
+  },
 });
 
 module.exports = {
