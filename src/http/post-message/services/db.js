e2514ced01e1d3db2f46ae8a77bddfa4c7cb722e
Sergii <sergiy.stotskiy@gmail.com>
2021-02-18 03:40:47 +0200

    fix: uses env vars

--- src/http/post-message/services/db.js
+++ src/http/post-message/services/db.js
@@ -1,7 +1,13 @@
+process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || process.env.BS_ACCESS_KEY_ID;
+process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || process.env.BS_SECRET_ACCESS_KEY;
+
 const { DynamoDB } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
-const db = new DynamoDB({ region: 'eu-central-1' });
+const db = new DynamoDB({
+  region: 'eu-central-1',
+
+});
 
 module.exports = {
   db,
