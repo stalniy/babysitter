b4b5c865b3584d0fd27f2c4c9fa06eac7138e6f8
Sergii <sergiy.stotskiy@gmail.com>
2021-02-18 03:57:57 +0200

    fix: tokens

--- src/http/post-message/services/db.js
+++ src/http/post-message/services/db.js
@@ -4,8 +4,8 @@ const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 const db = new DynamoDB({
   region: 'eu-central-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || process.env.BS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || process.env.BS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.BS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.BS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
