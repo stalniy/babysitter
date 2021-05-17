8c7a7ee904c39d63219646825199319f48f86581
Sergii <sergiy.stotskiy@gmail.com>
2021-05-17 10:54:49 +0300

    feat: adds total dream time during day to "baby" cmd

--- src/http/post-message/commands/baby.js
+++ src/http/post-message/commands/baby.js
@@ -22,13 +22,14 @@ function renderStandardRegime(ageInMonth) {
   const regime = getStandardRegimeAt(ageInMonth);
 
   if (!regime) {
-    return 'No regime items found for baby\'s age';
+    return `No regime items found for baby's age: ${ageInMonth} month`;
   }
 
   return `<b>Standard regime for ${regime.age.raw}</b>:\n`
     + `Waking time: ${regime.wakingTime.raw}\n`
-    + `Dream time during day: ${regime.dayDreamTime.raw}\n`
-    + `Night dream time: ${regime.nightDreamTime.raw}`;
+    + `1 dream time during day: ${regime.dayDreamTime.raw}\n`
+    + `total dream time during day: ${regime.totalDayDreamTime.raw}\n`
+    + `Night dream time: ${regime.nightDreamTime.raw}\n`;
 }
 
 module.exports = {
