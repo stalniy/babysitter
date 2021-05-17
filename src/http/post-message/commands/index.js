46cf8ec813e54dc557137dcddf33e92bbc3ae8d8
Sergii <sergiy.stotskiy@gmail.com>
2021-05-17 10:34:36 +0300

    fix: adds clear cache

--- src/http/post-message/commands/index.js
+++ src/http/post-message/commands/index.js
@@ -2,10 +2,12 @@ const start = require('./start');
 const status = require('./status');
 const events = require('./events');
 const baby = require('./baby');
+const clearCache = require('./clearCache');
 
 module.exports = {
   start,
   status,
   events,
   baby,
+  clearCache,
 };
